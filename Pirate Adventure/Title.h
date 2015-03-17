dfe867d3bcdf29a6c0bddb91cb495ef8b461d3f3
Jim Veneskey <github@strifecrafter.com>
2015-03-17 12:57:46 -0400

    Starting work on tiles

---
+++ Pirate Adventure/Title.h
@@ -0,0 +1,20 @@
+//
+//  Title.h
+//  Pirate Adventure
+//
+//  Created by jim Veneskey on 3/17/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Title : NSObject
+
+@property (strong, nonatomic) NSString *story;
+@property (strong, nonatomic) UIImage *backgroundImage;
+@property (strong, nonatomic) NSString *actionButtonName;
+
+
+
+
+@end
---
+++ Pirate Adventure/Title.m
@@ -0,0 +1,13 @@
+//
+//  Title.m
+//  Pirate Adventure
+//
+//  Created by jim Veneskey on 3/17/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import "Title.h"
+
+@implementation Title
+
+@end
